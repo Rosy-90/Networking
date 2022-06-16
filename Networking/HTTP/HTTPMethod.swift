3a25abcad1707c58b3b4a3444a086a82dcaf660b
Rosy Patel <rosypatel109@hotmail.com>
2022-06-16 15:13:03 +0100

    adds swift lint script

--- Networking/HTTP/HTTPMethod.swift
+++ Networking/HTTP/HTTPMethod.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public struct HTTPMethod: RawRepresentable, Equatable, Hashable {
     public let rawValue: String
-    
+
     public init?(rawValue: String) {
         self.rawValue = rawValue
     }
-    
+
     public static let get = HTTPMethod(rawValue: "GET")
     public static let post = HTTPMethod(rawValue: "POST")
 }
--- Networking/Service/Service.swift
+++ Networking/Service/Service.swift
@@ -11,38 +11,38 @@ protocol Servicable: JsonDecodable {
     func fetchData<T: Codable>(networkClient: NetworkClient, type: T.Type, completionHandler: @escaping Completion<T>)
 }
 
-typealias Completion<T:Decodable> =  ((Result<[T], HTTPError>) -> Void)
+typealias Completion<T: Decodable> =  ((Result<[T], HTTPError>) -> Void)
 
 public class Service: Servicable {
     public static let shared = Service()
-    
+
     func fetchData<T: Decodable>(networkClient: NetworkClient, type: T.Type, completionHandler: @escaping Completion<T>) {
         let urlSession = URLSession(configuration: .default)
         var dataTask: URLSessionDataTask?
-        
+
         guard let urlComponents = URLComponents(string: networkClient.baseURL) else {
             completionHandler(.failure(.urlFailed))
             return
         }
-        
+
         guard let url = urlComponents.url else {
             completionHandler(.failure(.urlFailed))
             return
         }
-        
+
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = networkClient.method
-                dataTask = urlSession.dataTask(with: urlRequest) { [weak self] (data, response, error) in
+                dataTask = urlSession.dataTask(with: urlRequest) { [weak self] (data, response, _) in
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                 completionHandler(.failure(.requestError))
                 return
             }
-            
+
             guard let data = data else {
                 completionHandler(.failure(.noData))
                 return
             }
-            
+
             if let result = self?.decode(input: data, type: type) {
                 completionHandler(.success(result))
             } else {
